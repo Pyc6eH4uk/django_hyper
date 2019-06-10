639b0a08e650531a806689086bfc222e357babb2
pyc6eh4uk <ruslan.klimov1@gmail.com>
2019-06-10 16:38:24 +0300

    Ordering

--- hyperlinks/models.py
+++ hyperlinks/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 
-MARKS = (
-    (1, 1),
-    (2, 2),
-    (3, 3),
-    (4, 4),
-    (5, 5)
-)
-
 
 class Subject(models.Model):
     title = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-title']
 
 
 class Student(models.Model):
@@ -22,3 +17,9 @@ class Student(models.Model):
     surname = models.CharField(max_length=100)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.subject.title}'
+
+
+    class Meta:
+        ordering = ['-id']
